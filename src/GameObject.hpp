1950123ad16a39b9e4e40da7d8eb35d0041abf63
John O'Donnell <johnoavocado@gmail.com>
2018-10-31 15:22:04 +0100

    Add Component and GameObject

---
+++ src/GameObject.hpp
@@ -0,0 +1,65 @@
+// Includes
+#include <memory>
+#include <vector>
+#include "glm/glm.hpp"
+#include "sre/SpriteBatch.hpp"
+#pragma once
+
+// Forward declaration
+class Component;
+
+class GameObject {
+    public:
+
+        ~GameObject();
+
+        template <class T>
+        std::shared_ptr<T> addComponent();
+
+        template <class T>
+        std::shared_ptr<T> getComponent();
+
+        bool removeComponent(std::shared_ptr<Component> component);
+
+        void renderSprite(sre::SpriteBatch::SpriteBatchBuilder& spriteBatchBuilder);
+        void update(float deltaTime);
+
+        const glm::vec2 &getPosition() const;
+
+        void setPosition(const glm::vec2 &position);
+
+        float getRotation() const;
+
+        void setRotation(float rotation);
+
+        const std::vector<std::shared_ptr<Component>>& getComponents();
+
+        std::string name = "_";
+    private:
+        GameObject() = default;
+        std::vector<std::shared_ptr<Component>> components;
+
+        glm::vec2 position;
+        float rotation;
+
+        friend class JunkDragonGame;
+};
+
+template <class T>
+inline std::shared_ptr<T> GameObject::addComponent(){
+    auto obj = std::shared_ptr<T>(new T(this));
+    components.push_back(obj);
+
+    return obj;
+}
+
+template <class T>
+inline std::shared_ptr<T> GameObject::getComponent(){
+    for (auto c : components){
+        std::shared_ptr<T> res = std::dynamic_pointer_cast<T>(c);
+        if (res != nullptr) {
+            return res;
+        }
+    }
+    return std::shared_ptr<T>();
+}
